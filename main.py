87d7fe49955811e0ab07f022a352dd75874f6bb7
Sergio Majé <smajefranco@gmail.com>
2021-05-16 03:09:02 -0500

    Cargar datos a la base de datos

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ from database import DataBase
 db = DataBase()
 
 
+def get_fee_values() -> Iterator[Fee]:
+    for value in range(50, 10_050, 50):
+        yield Fee(value=value)
+
+
+def load_db():
+    for fee in get_fee_values():
+        db.add_fee(fee)
+    set_config(True)
+
+
 def main():
     while True:
         x = input('''Sistema de Ahorro
@@ -15,7 +26,12 @@ def main():
         0. Salir
 > ''')
         if x == '1':
-            pass
+            if not config()['exists_db']:
+                print('Cargando base de datos...')
+                load_db()
+                print('La base de datos ha sido creada y cargada')
+            else:
+                print('La base de datos ya ha sido creada con anterioridad')
         elif x == '2':
             pass
         elif x == '0':
