79d000a8584ceae3854dc200f2f0bd918e96f2ca
Sergio Majé <smajefranco@gmail.com>
2021-05-16 01:04:31 -0500

    Tabla de la base de datos

---
+++ fee.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Boolean
+
+from base import Base
+
+
+class Fee(Base):
+    __tablename__ ='fee'
+
+    id = Column(Integer,
+                primary_key=True,
+                unique=True,
+                nullable=False,
+                sqlite_autoincrement=True)
+    value = Column(Integer, unique=True, nullable=False)
+    check = Column(Boolean, nullable=False, default=False)
