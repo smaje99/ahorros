bddaeaa4cbff6e56745ecbd8a09ba6a4730bd417
Sergio Majé <smajefranco@gmail.com>
2021-05-17 17:02:19 -0500

    Estructura del registro de cuotas

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
         x = input('''Sistema de Ahorro
         1. Crear base de datos
         2. Estadísticas
+        3. Registrar cuotas
         0. Salir
 > ''')
         if x == '1':
@@ -17,6 +18,23 @@ def main():
         elif x == '2':
             print(fd.money_statistics(), end='\n' * 2)
             print(fd.fees_statistics())
+        elif x == '3':
+            while True:
+                x = input('1. Ver tabla' + \
+                    '2. Registrar cuota' + \
+                    '3. Eliminar cuota' + \
+                    '0. Salir' + \
+                    '>')
+                if x == '1':
+                    pass
+                elif x == '2':
+                    pass
+                elif x == '3':
+                    pass
+                elif x == '0':
+                    print('Saliendo del registro de cuotas', end='\n' * 2)
+                else:
+                    print('Opción desconocida')
         elif x == '0':
             print('Cerrando aplicación...')
             break
