a8d67f9dde9d16b5ec9d1ba9bc2799e31c6f8cea
Sergio Majé <smajefranco@gmail.com>
2021-05-20 19:26:08 -0500

    Se aplicó los context manager del alcance de sesión a database.py

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
-from contextlib import contextmanager
 from typing import List
 
 from sqlalchemy.sql import func
 
-from base import Base, engine, Session
+from base import Base, engine, session_reading, session_writing
 from fee import Fee
 
 
@@ -11,97 +10,48 @@ class DataBase:
     def __init__(self):
         Base.metadata.create_all(engine)
 
-    @contextmanager
-    def __session_reading(self):
-        '''Proporciona un alcance transaccional en
-        torno a una serie de operaciones para lectura
-
-        Yields:
-            Session: serie de operaciones para lectura
-        '''
-        session = Session()
-        try:
-            yield session
-        except:
-            session.rollback()
-            raise
-        finally:
-            session.close()
-
-    @contextmanager
-    def __session_writing(self):
-        '''Proporciona un alcance transaccional en
-        torno a una serie de operaciones para escritura
-
-        Yields:
-            Session: serie de operaciones para escritura
-        '''
-        session = Session()
-        try:
-            yield session
-            session.commit()
-        except:
-            session.rollback()
-            raise
-        finally:
-            session.close()
-
     def add_fee(self, fee: Fee):
-        session = Session()
-        session.add(fee)
-        session.commit()
-        session.close()
+        with session_writing() as session:
+            session.add(fee)
 
     def get_fee(self, id: int) -> Fee:
-        session = Session()
-        result = (session.query(Fee)
-                    .filter(Fee.id == id)
-                    .first())
-        session.close()
-        return result
+        with session_reading() as session:
+            return (session.query(Fee)
+                        .filter(Fee.id == id)
+                        .first())
 
     def get_fees(self) -> List[Fee]:
-        session = Session()
-        result = session.query(Fee).all()
-        session.close()
-        return result
+        with session_reading() as session:
+            return session.query(Fee).all()
 
     def update_fee_check(self, id: int, check: bool):
-        session = Session()
-        (session.query(Fee)
-            .filter(Fee.id == id)
-            .update({Fee.check: check}))
-        session.commit()
-        session.close()
+        with session_writing() as session:
+            (session.query(Fee)
+                .filter(Fee.id == id)
+                .update({Fee.check: check}))
 
     def saved_money(self) -> int:
-        session = Session()
-        result = (session.query(func.sum(Fee.value))
-                        .filter(Fee.check)
-                        .one()[0])
-        session.close()
-        return result if result else 0
+        with session_reading() as session:
+            result = (session.query(func.sum(Fee.value))
+                            .filter(Fee.check)
+                            .one()[0])
+            return result if result else 0
 
     def missing_money(self) -> int:
-        session = Session()
-        result = (session.query(func.sum(Fee.value))
-                        .filter(Fee.check == False)
-                        .one()[0])
-        session.close()
-        return result if result else 0
+        with session_reading() as session:
+            result = (session.query(func.sum(Fee.value))
+                            .filter(Fee.check == False)
+                            .one()[0])
+            return result if result else 0
 
     def fees_checked(self) -> int:
-        session = Session()
-        result = (session.query(Fee)
-                        .filter(Fee.check)
-                        .count())
-        session.close()
-        return result
+        with session_reading() as session:
+            return (session.query(Fee)
+                            .filter(Fee.check)
+                            .count())
 
     def fees_not_checked(self) -> int:
-        session = Session()
-        result = (session.query(Fee)
-                        .filter(Fee.check == False)
-                        .count())
-        session.close()
-        return result
+        with session_reading() as session:
+            return (session.query(Fee)
+                            .filter(Fee.check == False)
+                            .count())
