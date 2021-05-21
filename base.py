7d4f45aaaf1d4c1fc1673ddd1aa46a41f9d61db3
Sergio Majé <smajefranco@gmail.com>
2021-05-20 19:25:06 -0500

    Se movió el alcalce de sesión de database.py a base.py

--- base.py
+++ base.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -8,3 +10,39 @@ engine = create_engine('sqlite:///ahorros.db')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+
+@contextmanager
+def session_reading():
+    '''Proporciona un alcance transaccional en
+    torno a una serie de operaciones para lectura
+
+    Yields:
+        Session: serie de operaciones para lectura
+    '''
+    session = Session()
+    try:
+        yield session
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+@contextmanager
+def session_writing():
+    '''Proporciona un alcance transaccional en
+    torno a una serie de operaciones para escritura
+
+    Yields:
+        Session: serie de operaciones para escritura
+    '''
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
